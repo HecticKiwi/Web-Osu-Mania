8b42463d192b95c479ede58b08bb1602775c27a8
Danny Duong <dannyduong4.00@outlook.com>
2025-03-17 10:47:30 -0600

    rtydty j

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,6 +6,8 @@ import { OSU_HEIGHT, OSU_WIDTH } from "@/osuMania/constants";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+export const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
--- src/osuMania/assets.ts
+++ src/osuMania/assets.ts
@@ -1,31 +1,18 @@
+import { BASE_PATH } from "@/lib/utils";
 import { Assets } from "pixi.js";
 
 export async function loadAssets() {
-  await Assets.load(`${process.env.NEXT_PUBLIC_BASE_PATH}/RobotoMono.ttf`);
-  await Assets.load(`${process.env.NEXT_PUBLIC_BASE_PATH}/VarelaRound.ttf`);
+  await Assets.load(`${BASE_PATH}/RobotoMono.ttf`);
+  await Assets.load(`${BASE_PATH}/VarelaRound.ttf`);
 
   await Promise.all([
-    await Assets.load(
-      `${process.env.NEXT_PUBLIC_BASE_PATH}/skin/mania-hit0-0.png`,
-    ),
-    await Assets.load(
-      `${process.env.NEXT_PUBLIC_BASE_PATH}/skin/mania-hit50-0.png`,
-    ),
-    await Assets.load(
-      `${process.env.NEXT_PUBLIC_BASE_PATH}/skin/mania-hit100-0.png`,
-    ),
-    await Assets.load(
-      `${process.env.NEXT_PUBLIC_BASE_PATH}/skin/mania-hit200-0.png`,
-    ),
-    await Assets.load(
-      `${process.env.NEXT_PUBLIC_BASE_PATH}/skin/mania-hit300-0.png`,
-    ),
-    await Assets.load(
-      `${process.env.NEXT_PUBLIC_BASE_PATH}/skin/mania-hit300g-0.png`,
-    ),
-    await Assets.load(`${process.env.NEXT_PUBLIC_BASE_PATH}/skin/arrow.svg`),
-    await Assets.load(
-      `${process.env.NEXT_PUBLIC_BASE_PATH}/skin/arrowOutline.svg`,
-    ),
+    await Assets.load(`${BASE_PATH}/skin/mania-hit0-0.png`),
+    await Assets.load(`${BASE_PATH}/skin/mania-hit50-0.png`),
+    await Assets.load(`${BASE_PATH}/skin/mania-hit100-0.png`),
+    await Assets.load(`${BASE_PATH}/skin/mania-hit200-0.png`),
+    await Assets.load(`${BASE_PATH}/skin/mania-hit300-0.png`),
+    await Assets.load(`${BASE_PATH}/skin/mania-hit300g-0.png`),
+    await Assets.load(`${BASE_PATH}/skin/arrow.svg`),
+    await Assets.load(`${BASE_PATH}/skin/arrowOutline.svg`),
   ]);
 }
