45599c064c96919dc863ffa011bc7c3e2af0b9b3
Danny Duong <dannyduong4.00@outlook.com>
2025-03-21 15:24:17 -0600

    Set default keybinds

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -86,6 +86,11 @@ export function getSettings() {
     ...settings.mods,
   };
 
+  settings.keybinds = {
+    ...defaultSettings.keybinds,
+    ...settings.keybinds,
+  };
+
   if (!settings.keybinds.keyModes[9]) {
     // Set default 10K keybinds
     settings.keybinds.keyModes[9] = [...defaultSettings.keybinds.keyModes[9]];
