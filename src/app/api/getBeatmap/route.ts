8e52fb7c56496ab425c7b7d9eac2b5b1a850151b
Danny Duong <dannyduong4.00@outlook.com>
2025-05-18 18:54:34 -0600

    sdfsdf

--- src/app/api/getBeatmap/route.ts
+++ src/app/api/getBeatmap/route.ts
@@ -1,7 +1,7 @@
 import { BeatmapSet } from "@/lib/osuApi";
 import { NextRequest, NextResponse } from "next/server";
 import { getAccessToken } from "../utils";
-//a
+
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
 
