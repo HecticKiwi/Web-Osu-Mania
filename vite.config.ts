0560574d263449a3ca7255e18f19512a98f04463
Danny Duong <dannyduong4.00@outlook.com>
2026-01-25 17:59:14 -0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+//
 export default defineConfig({
   server: {
     port: 3000,
