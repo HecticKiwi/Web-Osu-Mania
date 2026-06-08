b4955127e635e23dc8df8b095181118be25d76f6
Danny Duong <88013638+HecticKiwi@users.noreply.github.com>
2026-06-07 19:45:20 -0600

    Implement Proper Fade In/Out Effect (#245)

--- src/osuMania/sprites/stageCover.ts
+++ src/osuMania/sprites/stageCover.ts
@@ -1,4 +1,4 @@
-import { Container, Graphics } from "pixi.js";
+import { FillGradient, Graphics, RenderTexture, Sprite } from "pixi.js";
 import {
   arrowColumnRatio,
   circleColumnRatio,
@@ -9,17 +9,13 @@ import type { Game } from "../game";
 export class StageCover {
   public game: Game;
 
-  public view: Container;
-  public graphics: Graphics;
+  public view: Sprite;
 
   constructor(game: Game) {
     this.game = game;
 
-    this.graphics = new Graphics();
-
-    this.view = new Container();
+    this.view = new Sprite();
     this.view.zIndex = 2;
-    this.view.addChild(this.graphics);
   }
 
   public resize() {
@@ -28,7 +24,6 @@ export class StageCover {
     }
 
     let position = this.game.hitPosition;
-
     if (this.game.settings.style === "circles") {
       position += (this.game.scaledColumnWidth * circleColumnRatio) / 2;
     } else if (this.game.settings.style === "arrows") {
@@ -37,18 +32,96 @@ export class StageCover {
       position += (this.game.scaledColumnWidth * diamondColumnRatio) / 2;
     }
 
+    const graphics = new Graphics();
     const width = this.game.difficulty.keyCount * this.game.scaledColumnWidth;
-
-    let y: number;
     let height: number;
+
     if (this.game.mods.cover.type === "fadeIn") {
-      y = position * this.game.mods.cover.amount;
       height = this.game.app.screen.height;
+
+      // Solid portion
+      graphics
+        .clear()
+        .rect(0, position * (this.game.mods.cover.amount + 0.25), width, height)
+        .fill(0xffffff);
+
+      const fillGradient = new FillGradient({
+        start: {
+          x: 0,
+          y: 0,
+        },
+        end: {
+          x: 0,
+          y: 1,
+        },
+        colorStops: [
+          {
+            offset: 0,
+            color: "rgba(255, 0, 0, 0)",
+          },
+          {
+            offset: 1,
+            color: "rgba(255, 0, 0, 1)",
+          },
+        ],
+      });
+
+      // Gradient fade portion
+      graphics
+        .rect(0, position * this.game.mods.cover.amount, width, position * 0.25)
+        .fill(fillGradient);
     } else {
-      y = 0;
       height = position * (1 - this.game.mods.cover.amount);
+
+      // Solid portion
+      graphics
+        .clear()
+        .rect(0, 0, width, position * (1 - this.game.mods.cover.amount - 0.25))
+        .fill(0xffffff);
+
+      const fillGradient = new FillGradient({
+        start: {
+          x: 0,
+          y: 0,
+        },
+        end: {
+          x: 0,
+          y: 1,
+        },
+        colorStops: [
+          {
+            offset: 0,
+            color: "rgba(255, 0, 0, 1)",
+          },
+          {
+            offset: 1,
+            color: "rgba(255, 0, 0, 0)",
+          },
+        ],
+      });
+
+      // Gradient fade portion
+      graphics
+        .rect(
+          0,
+          position * (1 - this.game.mods.cover.amount - 0.25),
+          width,
+          position * 0.25,
+        )
+        .fill(fillGradient);
     }
 
-    this.graphics.clear().rect(0, y, width, height).fill(0x110000);
+    const texture = RenderTexture.create({
+      width,
+      height,
+      antialias: true,
+    });
+
+    this.game.app.renderer.render({
+      container: graphics,
+      target: texture,
+    });
+
+    this.view.texture = texture;
   }
 }
